5ae642284550cf4278a0e2c7301d53954a3cfe05
simfro01 <simon.frost@arm.com>
2025-09-02 12:12:01 +0100

    Fix CDDL test suite for cca-realm-mec-policy
    Signed-off-by: simfro01 <simon.frost@arm.com>

--- cddl/realm/frags.mk
+++ cddl/realm/frags.mk
@@ -9,6 +9,7 @@ REALM_FRAGS += cca-realm-personalization-value.cddl
 REALM_FRAGS += cca-realm-profile.cddl
 REALM_FRAGS += cca-realm-public-key-hash-algo-id.cddl
 REALM_FRAGS += cca-realm-public-key.cddl
+REALM_FRAGS += cca-realm-mec-policy.cddl
 REALM_FRAGS += COSE_Key.cddl
 
 REALM_EXAMPLES := $(wildcard examples/*.diag)
