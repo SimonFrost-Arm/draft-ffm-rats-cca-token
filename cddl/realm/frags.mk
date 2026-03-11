4482c6b25e5738c72394a10ab8d6fc034900dacc
simfro01 <simon.frost@arm.com>
2026-03-11 00:18:11 +0000

    Further claims updates incl missing realm claims
    Signed-off-by: simfro01 <simon.frost@arm.com>

--- cddl/realm/frags.mk
+++ cddl/realm/frags.mk
@@ -10,6 +10,10 @@ REALM_FRAGS += cca-realm-profile.cddl
 REALM_FRAGS += cca-realm-public-key-hash-algo-id.cddl
 REALM_FRAGS += cca-realm-public-key.cddl
 REALM_FRAGS += cca-realm-mec-policy.cddl
+REALM_FRAGS += cca-realm-lfa-policy.cddl
+REALM_FRAGS += cca-realm-instance-id.cddl
+REALM_FRAGS +=  cca-realm-devices-token-hash.cddl
 REALM_FRAGS += COSE_Key.cddl
+REALM_FRAGS += ../platform/arm-platform-hash-types.cddl
 
 REALM_EXAMPLES := $(wildcard examples/*.diag)
